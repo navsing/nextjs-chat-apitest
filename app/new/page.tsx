34ff1d971d1192cbf2a170d8e52a86b8ec5cdb82
Amit Kesarwani <93291915+kesarwam@users.noreply.github.com>
2024-10-23 18:41:40 -0700

    Delete user session data when starting new chat

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,5 +1,21 @@
 import { redirect } from 'next/navigation'
 
 export default async function NewPage() {
+  const device = {
+    DeviceID: "web123"
+  };
+  let response = await fetch(process.env.HEADACHE_AI_API_CLEAR_SESSION, {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      "api-key": process.env.HEADACHE_AI_API_KEY,
+    },
+    body: JSON.stringify(device),
+    cache: 'no-store'
+  })
+  let response_json = await response.json()
+  console.log(JSON.stringify(response_json))
+
   redirect('/')
 }
